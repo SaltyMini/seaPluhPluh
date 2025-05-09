ee0224eb656a31825fe2e141e112fe326c113f52
SaltyMini <samclaymichael@gmail.com>
2025-05-09 03:17:17 +0100

    added vectorContaines util

---
+++ Source Files/utils/utils.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+
+std::string toLowerString(const std::string& input) {
+    std::string result = input;
+    for (char& c : result) {
+        c = std::tolower(c);
+    }
+    return result;
+}
+
+std::string toUpperString(const std::string& input) {
+    std::string result = input;
+    for (char& c : result) {
+        c = std::toupper(c);
+    }
+    return result;
+}
+
+bool vectorContains(const std::vector<std::string>& vec, const std::string& str) {
+	for (const auto& item : vec) {
+		if (item == str) {
+			return true;
+		}
+	}
+	return false;
+}
