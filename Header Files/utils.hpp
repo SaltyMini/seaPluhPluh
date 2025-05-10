2e38caa5310d9a88e2d0386f96cf493803517ec0
SaltyMini <samclaymichael@gmail.com>
2025-05-10 02:57:20 +0100

    created header for utils

---
+++ Header Files/utils.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+// String manipulation functions
+std::string toLowerString(const std::string& input);
+std::string toUpperString(const std::string& input);
+
+// Vector utility functions
+bool vectorContains(const std::vector<std::string>& vec, const std::string& str);
